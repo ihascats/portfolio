e5c40b00f07fa604f8a1a0d4cde432e430a1a770
ihascats <s.pavlovic.rs@gmail.com>
2022-08-06 13:52:31 +0200

    Add skeleton of the project

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,107 @@
 import '../css/styles.css';
+import Phaser from 'phaser';
+import playerSheet from '../assets/playerSpriteSheet/player.png';
+import blue from '../assets/background/blue.png';
+import grass from '../assets/platform/tile_0022.png';
+import show from '../assets/sign.png';
+
+const width = 600;
+const height = 200;
+
+function preload() {
+  this.load.image('sky', blue);
+  this.load.image('grass', grass);
+  this.load.image('sign', show);
+  this.load.spritesheet('player', playerSheet, {
+    frameWidth: 20,
+    frameHeight: 23,
+  });
+}
+let player;
+let platform;
+let cursors;
+let sign;
+
+function showProject(character, target) {
+  target.disableBody(false, false);
+}
+
+function create() {
+  this.cameras.main.setBounds(0, 0, width * 2, height);
+  this.physics.world.setBounds(0, 0, width * 2, height, true, true, true, true);
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  sign = this.physics.add.group({
+    key: 'sign',
+    repeat: 6,
+    setXY: { x: 8, y: 0, stepX: 120 },
+  });
+  player = this.physics.add.sprite(100, 150, 'player');
+
+  sign.children.iterate((child) => {
+    child.setBounceY(0.4);
+  });
+
+  this.physics.add.overlap(player, sign, showProject, null, this);
+
+  this.cameras.main.startFollow(player, true, 0.09, 0.09);
+  platform = this.physics.add.staticGroup();
+  for (let i = 0; i < (width * 2) / 18; i += 1) {
+    platform.create(9 + 18 * i, height - 9, 'grass');
+  }
+  player.setCollideWorldBounds(true);
+  player.body.setGravityY(300);
+  this.physics.add.collider(player, platform);
+  this.physics.add.collider(sign, platform);
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', { start: 3, end: 2 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+}
+
+function update() {
+  cursors = this.input.keyboard.createCursorKeys();
+  const velocity = 300;
+  if (cursors.left.isDown) {
+    player.setVelocityX(velocity * -1);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(velocity);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.stop();
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(velocity * -1);
+  }
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width,
+  height,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+const game = new Phaser.Game(config);
